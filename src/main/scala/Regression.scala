64ed18e0857f0e2056a83706c6b842edee14b8bb
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-03-31 16:19:09 +0300

    cod rescris

--- src/main/scala/Regression.scala
+++ src/main/scala/Regression.scala
@@ -10,77 +10,70 @@ object Regression {
                 gradient_descent_steps: Int): (Matrix, Double) = {
 
     // 3.1 Incarcarea si selectia datelor
-    val ds: Dataset = Dataset.apply(dataset_file)
+    val ds = Dataset.apply(dataset_file)
 
-    val (trainSet, evalSet) = ds.split(test_percentage)
+    val datasetX = ds.selectColumns(attribute_columns)
+    val datasetY = ds.selectColumn(value_column)
 
-
-    val selectedX: Dataset = ds.selectColumns(attribute_columns)
-    val selectedY: Dataset = ds.selectColumn(value_column)
-
-    val (trainX, evalX) = selectedX.split(test_percentage)
-    val (trainY, evalY) = selectedY.split(test_percentage)
-
+    val (trainDS_X, evalDS_X) = datasetX.split(test_percentage)
+    val (trainDS_Y, evalDS_Y) = datasetY.split(test_percentage)
 
     // 3.2 Crearea matricii de antrenare X
-    val matriceValoriAntrenare: Matrix = Matrix.apply(trainX)
+    // le vom folosi pentru calculul vectorului `W`
+    // coeficientii lui `x` ai dreptei
+    val X_train: Matrix = Matrix.apply(trainDS_X)
+    val Y_train: Matrix = Matrix.apply(trainDS_Y)
 
-    val (m: Int, n: Int) = matriceValoriAntrenare.getDimensions()
+    val (m: Int, n: Int) = X_train.getDimensions()
 
-    val X: Matrix = matriceValoriAntrenare ++ 1    // m x (n + 1)
-
-
-
-
+    val X_train_mat: Matrix = X_train ++ 1
 
     // 3.3 Initializarea coeficientilor regresiei / ipotezei (W)
     val W_init: Matrix = Matrix(List.fill(n + 1)(List.fill(1)(0.0)))
-
-    // matrice m * 1
-    val Y_real_train: Matrix = Matrix(trainY)
-
 
     /**
      * 3.4 Gradient Descent
      */
     @tailrec
-    def gradient_heleper(W_step: Matrix, current_step: Int): Matrix = {
-      if (current_step <= 0) W_step
+    def gradient_descent_helper(W_step: Matrix, step: Int) : Matrix = {
+      if (step <= 0) W_step
       else {
         // TODO: cerinta 4
         // X -> matrice m * (n + 1)
         // W_step -> matrice (n + 1) * 1
 
         // Y_estimat -> matrice m * 1
-        val Y_estimat_train: Matrix = X * W_step
+        val Y_estimat: Matrix = X_train_mat * W_step
 
         // err -> matrice m * 1
-        val err_vector: Matrix = (Y_real_train - Y_estimat_train).map(el => Math.abs(el))
+        val err_vec: Matrix = (Y_estimat - Y_train).map(el => el.abs)
 
         // gradient -> matrice (n + 1) * 1
-        val gradient: Matrix = (X.transpose * err_vector).map(el => el / m.toDouble)
+        val gradient: Matrix = (X_train_mat.transpose * err_vec).map(el => el / m.toDouble)
 
         val W_new: Matrix = W_step - gradient.map(el => el * alpha)
 
-        gradient_heleper(W_new, current_step - 1)
+        gradient_descent_helper(W_new, step - 1)
       }
-
     }
 
 
-    val W_final: Matrix = gradient_heleper(W_init, gradient_descent_steps)
+    val W_final: Matrix = gradient_descent_helper(W_init, gradient_descent_steps)
 
     // 3.5 calculul errorii
     // vedem cat de bine trece dreapta prin punctele de evaluare
-    val X_eval: Matrix = Matrix(evalX)
-    val Y_real_eval: Matrix = Matrix(evalY)
 
+    // le vom folosi pentru calculul erorii
+    val X_eval: Matrix = Matrix.apply(evalDS_X)
+    val Y_eval: Matrix = Matrix.apply(evalDS_Y)
 
-    val Y_predictie: Matrix = (X_eval ++ 1) * W_final
+    val X_eval_mat: Matrix = X_eval ++ 1
+    val Y_predicite: Matrix = X_eval_mat * W_final
 
-    val error_value: Double = Regression.sumAbsDiff(Y_real_eval, Y_predictie)
+     // media aritmetica intre modulul diferentei dintre pretul prezis si cel real
+    val err_val: Double = sumAbsDiff(Y_predicite, Y_eval) / m.toDouble
 
-    (W_final, error_value)
+    (W_final, err_val)
   }
 
 
